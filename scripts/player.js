b7e3d974fb2c664571d0975a6c4513494849eab2
Pablolo <pablo_olondriz@hotmail.com>
2020-01-27 17:56:00 +0100

    set up screen boundaries for player

--- scripts/player.js
+++ scripts/player.js
@@ -11,15 +11,31 @@ class Player {
   goLeft() {
     this.deltaX -= 10;
     console.log(this.deltaX);
-    ctx.fillRect(this.x + this.deltaX, this.y, this.width, this.height);
-    ctx.fillStyle = this.color;
+    let totalDelta = this.x + this.deltaX;
+    console.log(totalDelta);
+    if (totalDelta < 0) {
+      this.deltaX += 10;
+      ctx.fillRect(this.x + this.deltaX, this.y, this.width, this.height);
+      ctx.fillStyle = this.color;
+    } else {
+      ctx.fillRect(totalDelta, this.y, this.width, this.height);
+      ctx.fillStyle = this.color;
+    }
   };
 
   goRight() {
     this.deltaX += 10;
     console.log(this.deltaX);
-    ctx.fillRect(this.x + this.deltaX, this.y, this.width, this.height);
-    ctx.fillStyle = this.color;
+    let totalDelta = this.x + this.deltaX;
+    console.log(totalDelta);
+    if (totalDelta > 480) {
+      this.deltaX -= 10;
+      ctx.fillRect(this.x + this.deltaX, this.y, this.width, this.height);
+      ctx.fillStyle = this.color;
+    } else {
+      ctx.fillRect(totalDelta, this.y, this.width, this.height);
+      ctx.fillStyle = this.color;
+    }
   };
  
   // move();
